418f91049d6932965c031f7d931363533946249b
mj4oss <mjws4oss@gmail.com>
2025-12-10 13:34:22 -0800

    add a function to perform sum of two ints (#13)

--- src/simple_python_library/utils.py
+++ src/simple_python_library/utils.py
@@ -1,3 +1,8 @@
 def hello(name: str) -> str:
     """Return a greeting message for the given name."""
     return f"Hello, {name}!"
+
+
+def sum(a: int, b: int) -> int:
+    """Return the sum of two integers."""
+    return a + b
